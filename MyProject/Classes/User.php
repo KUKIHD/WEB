be6abf4ba4c15d7e28c928aaa8c914f6536494ca
KUKIHD <106991810+KUKIHD@users.noreply.github.com>
2025-04-18 23:47:49 +0300

    Add files via upload

---
+++ MyProject/Classes/User.php
@@ -0,0 +1,32 @@
+<?php
+// MyProject/Classes/User.php
+namespace MyProject\Classes;
+
+class User {
+    public $name;
+    public $login;
+    private $password;
+    public static $count = 0;
+
+    public function __construct($name, $login, $password) {
+        $this->name = $name;
+        $this->login = $login;
+        $this->password = $password;
+        if (get_class($this) === User::class) {
+            self::$count++;
+        }
+    }
+
+    public function showInfo() {
+        echo "<pre>Информация о пользователе:\n";
+        echo "Имя: $this->name | Логин: $this->login";
+    }
+
+    public static function getUserCount() {
+        return self::$count;
+    }
+
+    public function __destruct() {
+        echo "Пользователь $this->login удален.\n";
+    }
+}
